155a6d0c3b20f13298872a313cf3ee56d6b73574
Manuel Dominguez <dominguezguerrero1996@gmail.com>
2024-05-27 19:36:14 -0500

    Home Secciones y blogs

--- PrivateBlog.Web/Controllers/HomeController.cs
+++ PrivateBlog.Web/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PrivateBlog.Web.Core;
+using PrivateBlog.Web.Core.Pagination;
+using PrivateBlog.Web.Data.Entities;
+using PrivateBlog.Web.DTOs;
 using PrivateBlog.Web.Models;
+using PrivateBlog.Web.Services;
 using System.Diagnostics;
 
 namespace PrivateBlog.Web.Controllers
@@ -9,32 +14,59 @@ namespace PrivateBlog.Web.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IHomeService _homeService;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger, IHomeService homeService)
+        {
+            _logger = logger;
+            _homeService = homeService;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public async  Task<IActionResult> Dashboard([FromQuery] int? RecordsPerPage,
+									   [FromQuery] int? Page,
+									   [FromQuery] string? Filter)
+        {
+            PaginationRequest request = new PaginationRequest
+            {
+                RecordsPerPage = RecordsPerPage ?? 15,
+                Page = Page ?? 1,
+                Filter = Filter
+            };
 
-		public IActionResult Dashboard()
-		{
-			return View();
-		}
+			Response<PaginationResponse<Section>> response = await _homeService.GetSectionsAsync(request);
 
-		[AllowAnonymous]
+            return View(response.Result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Section([FromRoute] int id,
+                                                 [FromQuery] int? RecordsPerPage,
+                                                 [FromQuery] int? Page,
+                                                 [FromQuery] string? Filter)
+        {
+            PaginationRequest request = new PaginationRequest
+            {
+                RecordsPerPage = RecordsPerPage ?? 15,
+                Page = Page ?? 1,
+                Filter = Filter
+            };
+
+            Response<SectionDTO> response = await _homeService.GetSectionAsync(request, id);
+
+            return View(response.Result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Blog([FromRoute] int id)
+        {
+            Response<Blog> response = await _homeService.GetBlogAsync(id);
+            return View(response.Result);
+        }
+
+        [AllowAnonymous]
 		public IActionResult Privacy()
 		{
 			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 	}
 }
