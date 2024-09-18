0b74333c09e5bf1ff1e3d762c99f4d6284b34a49
Manuel Dominguez <dominguezguerrero1996@gmail.com>
2024-09-18 02:14:02 -0500

    tamplate

---
+++ PrivateBlog.Web/Controllers/SectionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrivateBlog.Web.Data.Entities;
+
+namespace PrivateBlog.Web.Controllers
+{
+    public class SectionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Section> sections = new List<Section> 
+            {
+                new Section
+                {
+                    Id = 1,
+                    Name = "General",
+                },
+                
+                new Section
+                {
+                    Id = 2,
+                    Name = "Telecomunicaciones",
+                },
+                
+                new Section
+                {
+                    Id = 3,
+                    Name = "Hacking",
+                },
+            };
+
+            return View(sections);
+        }
+    }
+}
