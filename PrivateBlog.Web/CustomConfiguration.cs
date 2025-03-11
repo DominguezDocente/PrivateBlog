bdbc9cfd14960db77099a947c7e94e2a73c976e1
Manuel Dominguez <dominguezguerrero1996@gmail.com>
2025-03-10 19:51:34 -0500

    Dbcontext y migración de sections

---
+++ PrivateBlog.Web/CustomConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrivateBlog.Web.Data;
+using System.Runtime.CompilerServices;
+
+namespace PrivateBlog.Web
+{
+    public static class CustomConfiguration
+    {
+
+        public static WebApplicationBuilder AddCustomConfiguration(this WebApplicationBuilder builder)
+        {
+            // Data Context
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection"));
+            });
+
+            return builder;
+        }
+    }
+}
