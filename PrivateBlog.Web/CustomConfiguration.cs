c651eb3f72bdccd548f219f54271978a0bdd22aa
Manuel D <dominguez.docente@gmail.com>
2025-09-17 19:04:41 -0500

    Seeders Stuff and sections seeder

--- PrivateBlog.Web/CustomConfiguration.cs
+++ PrivateBlog.Web/CustomConfiguration.cs
@@ -2,6 +2,7 @@
 using AspNetCoreHero.ToastNotification.Extensions;
 using Microsoft.EntityFrameworkCore;
 using PrivateBlog.Web.Data;
+using PrivateBlog.Web.Data.Seeders;
 using PrivateBlog.Web.Services.Abtractions;
 using PrivateBlog.Web.Services.Implementations;
 
@@ -37,13 +38,25 @@ namespace PrivateBlog.Web
         private static void AddServices(WebApplicationBuilder builder)
         {
             builder.Services.AddScoped<ISectionsService, SectionsService>();
+            builder.Services.AddTransient<SeedDb>();
         }
 
         public static WebApplication AddCustomWebApplicationConfiguration(this WebApplication app)
         {
             app.UseNotyf();
 
+            SeedData(app);
+
             return app;
+        }
+
+        private static void SeedData(WebApplication app)
+        {
+            IServiceScopeFactory scopeFactory = app.Services.GetService<IServiceScopeFactory>();
+
+            using IServiceScope scope = scopeFactory.CreateScope();
+            SeedDb service = scope.ServiceProvider.GetService<SeedDb>();
+            service.SeedAsync().Wait();
         }
     }
 }
---
+++ PrivateBlog.Web/Data/Seeders/SectionsSeeder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrivateBlog.Web.Data.Entities;
+
+namespace PrivateBlog.Web.Data.Seeders
+{
+    public class SectionsSeeder
+    {
+        private readonly DataContext _context;
+
+        public SectionsSeeder(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            List<Section> sections = new List<Section>() 
+            {
+                new Section { Id = Guid.NewGuid(), Name = "General", Description = "Información base de la compañia"},
+                new Section { Id = Guid.NewGuid(), Name = "Informática"},
+                new Section { Id = Guid.NewGuid(), Name = "Pentesting"},
+                new Section { Id = Guid.NewGuid(), Name = "Clases", IsHidden = true}
+            };
+
+            foreach (Section section in sections) 
+            {
+                bool exists = await _context.Sections.AnyAsync(s => s.Id == section.Id);
+
+                if (!exists)
+                {
+                    await _context.Sections.AddAsync(section);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
